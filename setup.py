9ed38b702af22ab9416d83bccd3e7490099b3125
Ondra Machacek <omachace@redhat.com>
2016-06-28 14:17:17 +0200

    Add modules back to repo as readthedocs don't have maven

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ import sys
 from subprocess import Popen
 
 
-# Build modules documentation
-if "install" in sys.argv and os.environ.get('READTHEDOCS'):
+# This is just a hack how to generate modules and it's documentation
+# after push to git in the readthedocs.
+
+
+def make_modules():
     proc = Popen(['make', 'modules'], cwd='docs/')
     (_, err) = proc.communicate()
     return_code = proc.wait()
 
     if return_code or err:
-        raise Exception('Failed to generate doc: %s' % err)
+        raise Exception('Failed to generate modules doc: %s' % err)
+
+
+if "install" in sys.argv and os.environ.get('READTHEDOCS'):
+    make_modules()
